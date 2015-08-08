c09dc6b353775e8b37b83814d01c80446ccf20a3
Matt Layher <mdlayher@gmail.com>
2015-08-08 12:15:20 -0400

    cmd/wol: initial commit

---
+++ cmd/wol/main.go
@@ -0,0 +1,47 @@
+// Command wol is a simple Wake-on-LAN client.
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	"github.com/mdlayher/wol"
+)
+
+var (
+	ifaceFlag    = flag.String("i", "eth0", "network interface to use to send Wake-on-LAN magic packet")
+	targetFlag   = flag.String("t", "", "target for Wake-on-LAN magic packet")
+	passwordFlag = flag.String("p", "", "optional password for Wake-on-LAN magic packet")
+)
+
+func main() {
+	flag.Parse()
+
+	// Create client bound to specified interface
+	c, err := wol.NewClient(*ifaceFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Validate hardware address
+	addr, err := net.ParseMAC(*targetFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set password if one is present
+	var password []byte
+	if *passwordFlag != "" {
+		password = []byte(*passwordFlag)
+	}
+
+	// Attempt to wake target machine
+	if err := c.WakePassword(addr, password); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("sent Wake-on-LAN magic packet using %s to %s", *ifaceFlag, *targetFlag)
+
+	_ = c.Close()
+}
